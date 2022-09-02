e4ac2a067b5624c076eae7dc400f35a199a7eb05
sachin-kiwi <sachin.bisht@kiwitech.com>
2022-09-02 11:50:33 +0530

    feature - send transaction request to sign and submit using QR code

--- src/index.js
+++ src/index.js
@@ -9,6 +9,23 @@ const main = async () => {
     // quota -rateLimit
     const appInfo = await Sdk.ping()
     console.log(`Ping App: ${appInfo.application.name}`)
+    const request = {
+        TransactionType: 'Payment',
+        Destination: 'rwietsevLFg8XSmG3bEZzFein1g8RBqWDZ',
+        Amount: '10000',
+        Memos: [
+            {
+                Memo: {
+                    MemoData: 'F09F988E20596F7520726F636B21',
+                },
+            },
+        ],
+    }
+
+    const payload = await Sdk.payload.create(request, true)
+    const {refs={},next={}} = payload
+    console.log(`QR Code link: ${refs.qr_png}`)
+    console.log(`Xumm App link for sign: ${next?.always}`)
 }
 
 main()
